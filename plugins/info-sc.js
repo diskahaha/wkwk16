c482a730889cd20ea632591c58fe95a9eca50afd
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-07-27 22:57:32 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -12,7 +12,7 @@ let esce = `
 Hai ${ye} Lagi Nyari Sc Ya? 
 
 
-_*https://youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw*_`
+_*Yahahaha 🤧*_`
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
 handler.help = ['sc', 'sourcecode']
